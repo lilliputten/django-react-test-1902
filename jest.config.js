bf5ab2f6f89aa6b79c4ad6cd6677f3326f19dbe3
lilliputten <lilliputten@yandex.ru>
2019-02-03 21:55:21 +0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testURL : 'http://localhost',
+  testPathIgnorePatterns : ['node_modules'],
+  moduleFileExtensions : ['ts', 'tsx', 'js'],
+  // globals: {
+  //   __TS_CONFIG__: "tsconfig.test.json"
+  // },
+  transform : {
+    '^.+\\.(ts|tsx)$' : 'ts-jest',
+  },
+  testMatch : [
+      '**/src/**/*.test.+(ts|tsx)',
+  ],
+  moduleNameMapper: {
+    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|svg)$': '<rootDir>/__mocks__/style.ts'
+  },
+  // testEnvironment: '<rootDir>/__mocks__/customizedJsdomEnv.js',
+  setupFiles: [ '<rootDir>/__mocks__/setup.js', ],
+  modulePaths : [
+    '<rootDir>/src',
+  ],
+};
