12d098ff01e0e1f6102ee67b7db423fe63cb746b
lilliputten <lilliputten@yandex.ru>
2019-02-03 23:22:42 +0300

    django settings

---
+++ default_settings.py
@@ -0,0 +1,3 @@
+from django_project.settings import *
+
+ALLOWED_HOSTS=['django-test-1902.lilliputten.ru', 'www.django-test-1902.lilliputten.ru', 'django-test-1902.lilliputten.ru.fstest.ru', 'www.django-test-1902.lilliputten.ru.fstest.ru']
