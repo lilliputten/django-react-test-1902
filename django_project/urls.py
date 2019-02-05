89a1f88f1b3dbe9d56354c44045cfe773f16294c
lilliputten <lilliputten@yandex.ru>
2019-02-06 01:40:08 +0300

    urls, basic templates

--- django_project/urls.py
+++ django_project/urls.py
@@ -12,9 +12,11 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
