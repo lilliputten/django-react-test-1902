4990f493e9bd487f212a3290bdc333a1d7fc6b2c
lilliputten <lilliputten@yandex.ru>
2019-02-06 16:56:44 +0300

    node-test

---
+++ node-test.js
@@ -0,0 +1,1 @@
+﻿console.log('node-test result');
--- react1/views.py
+++ react1/views.py
@@ -6,16 +6,29 @@ from django.http import HttpResponse
 # from django.shortcuts import get_object_or_404, render
 # from .models import Question
 
+import os.path
+
 import subprocess
+from nodejs.bindings import node_run
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
 
 
 def index(request):
-    return HttpResponse("index")
+    return HttpResponse(
+        "CWD: " + CWD + "<br/>\n" +
+        "PROJECT_ROOT: " + PROJECT_ROOT + "<br/>\n" +
+        ""
+    )
 
 
 def test(request):
-    # subprocess.call(["node", "-v"])
-    p = subprocess.Popen(["node", "-v"], stdout=subprocess.PIPE)
+    script_name = os.path.join(SITE_ROOT, 'node-test.js')
+    # stderr, stdout = node_run(script_name, '--some-argument')
+    # p = subprocess.Popen(["node", "-v"], stdout=subprocess.PIPE)
+    p = subprocess.Popen(["node", script_name], stdout=subprocess.PIPE)
     result = p.communicate()
     output = result[0]
-    return HttpResponse(output)
+    return HttpResponse(script_name + ' => ' + output)
