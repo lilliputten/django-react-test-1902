4e20a9ab619573cbf92166266ab3be201613807a
lilliputten <lilliputten@yandex.ru>
2019-02-11 01:41:53 +0300

    react-build auto commit

--- react-build/Contacts-d6849147.js
+++ react-build/Contacts-d6849147.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{28:function(n,t,e){"use strict";e.r(t);var a=e(1);t.default=function(){return a.createElement("div",{className:"Contacts"},"Contacts")}}}]);
-//# sourceMappingURL=Contacts-4f68e7d2.js.map
+//# sourceMappingURL=Contacts-d6849147.js.map
